5c495565db39e2bb40fcae41cce6f03ca7700a5c
MarceloCLopes <marc01mcl@gmail.com>
2021-04-19 17:15:32 -0300

    created the methods components

--- app/src/main/java/com/example/appbarlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/appbarlayoutdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.appbarlayoutdemo;
 
+import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
